36c9bb1cfecc590396cc8c3410b677e0f6fdc043
GeorgeSalari <x3m.giku@gmail.com>
2017-08-24 00:22:23 +0800

    add directory for student.rb and teacher.rb

--- db/config.rb
+++ db/config.rb
@@ -1,3 +1,6 @@
 require 'active_record'
+require 'faker'
+require_relative '../app/models/student.rb'
+require_relative '../app/models/teacher.rb'
 
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => "#{File.dirname(__FILE__)}/../db/interface-students.sqlite3")
